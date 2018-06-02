d60b571ff8bbe64b1aed8d3edd381816ef907bd8
Sliim <sliim@mailoo.org>
2018-06-02 15:42:30 +0200

    Bump 0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,11 @@ autossh CHANGELOG
 
 This file is used to list changes made in each version of the autossh cookbook.
 
+0.2.0
+-----
+- Reload systemd daemon and restart service on service template change
+- Add enabled resource property
+
 0.1.0
 -----
 - Initial release of autossh cookbook
--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ maintainer_email 'sliim@mailoo.org'
 license 'Apache-2.0'
 description 'Deploy autossh services'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.2.0'
 chef_version '>= 12.0' if respond_to?(:chef_version)
 
 recipe 'autossh::default', 'Install autossh package'
